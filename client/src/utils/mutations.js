e3983ec08b9bfaaf6f866cb4d796d18fab1d8c79
quinncombs <qcombs31@gmail.com>
2024-01-25 11:29:40 -0500

    added queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,46 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql `
+    mutation saveBook($bookId: String!, $tiel: String!, $description: String!, $authors: [String], $image: String, $link: String!) {
+        saveBook(bookId: $bookId, description: $description, title: $title, authors: $authors, image: $image, link: $link) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
+`;
